d052ff2479a2ffa876717314146f34b3d6f9d90f
Shironim <dimasseto.id@gmail.com>
2023-05-04 14:14:24 +0700

    implement component

---
+++ components/Categories.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="md:max-w-5xl mx-auto mb-12">
+    <div class="flex">
+      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Kamera</span>
+      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Lensa</span>
+      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Aksesoris</span>
+      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Lampu</span>
+    </div>
+  </section>
+</template>
---
+++ components/ComponentProduct.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  dataProduct: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+<template>
+  <div v-for="(camera, index) in dataProduct" :key="index" class="basis-1/4">
+    <div class="border rounded-xl m-2">
+      <figure>
+        <NuxtImg :src="camera.img" :alt="camera.name" fit="cover" />
+      </figure>
+      <figcaption class="flex flex-col">
+        <div class="p-4">
+          <h1 class="text-lg font-bold">{{ camera.name }}</h1>
+          <p class="text-sm">Rp. {{ camera.priceDay }} / Hari</p>
+        </div>
+        <NuxtLink :to="`detail/${camera.slug}`" class="mt-auto">
+          <button class="border p-2 rounded-md text-md"> Detail </button>
+        </NuxtLink>
+      </figcaption>
+    </div>
+  </div>
+</template>
