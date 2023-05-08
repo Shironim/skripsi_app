85347802e34a73d3933f2e5c7c4b7785bd21fea7
Shironim <dimasseto.id@gmail.com>
2023-05-08 13:25:11 +0700

    change photo dir

--- server/api/camera.ts
+++ server/api/camera.ts
@@ -3,62 +3,62 @@ export default defineEventHandler((event) => {
     {
       slug: 'camera-eos-700d',
       name: 'Camera EOS 700D',
-      img : '/img/canon-eos-700d.png',
+      img : '/images/canon-eos-700d.png',
       priceDay : 100000,
       type: 'camera'
     },
     {
       slug: 'camera-eos-1200d',
       name: 'Canon EOS 1200D',
-      img : '/img/canon-eos-1200d.png',
+      img : '/images/canon-eos-1200d.png',
       priceDay : 180000
     },
     {
       slug: 'canon-650d',
       name: 'Canon 650D',
-      img : '/img/kamera-canon-650d.png',
+      img : '/images/kamera-canon-650d.png',
       priceDay : 80000
     },
     {
       slug: 'lensa-canon-f3.5-4.5',
       name: 'Lensa Canon f3.5-4.5',
-      img : '/img/lensa-canon-f3.5-4.5.png',
+      img : '/images/lensa-canon-f3.5-4.5.png',
       priceDay : 120000
     },
     {
       slug : 'lensa-nikon-50mm',
       name: 'Lensa Nikon 50mm',
-      img : '/img/lensa-nikon-af-s-50mm-f1.8g.jpg',
+      img : '/images/lensa-nikon-af-s-50mm-f1.8g.jpg',
       priceDay : 110000
     },
     {
       slug: 'camera-eos-700d',
       name: 'Camera EOS 700D',
-      img : '/img/canon-eos-700d.png',
+      img : '/images/canon-eos-700d.png',
       priceDay : 100000
     },
     {
       slug: 'camera-eos-1200d',
       name: 'Canon EOS 1200D',
-      img : '/img/canon-eos-1200d.png',
+      img : '/images/canon-eos-1200d.png',
       priceDay : 180000
     },
     {
       slug: 'canon-650d',
       name: 'Canon 650D',
-      img : '/img/kamera-canon-650d.png',
+      img : '/images/kamera-canon-650d.png',
       priceDay : 80000
     },
     {
       slug: 'lensa-canon-f3.5-4.5',
       name: 'Lensa Canon f3.5-4.5',
-      img : '/img/lensa-canon-f3.5-4.5.png',
+      img : '/images/lensa-canon-f3.5-4.5.png',
       priceDay : 120000
     },
     {
       slug : 'lensa-nikon-50mm',
       name: 'Lensa Nikon 50mm',
-      img : '/img/lensa-nikon-af-s-50mm-f1.8g.jpg',
+      img : '/images/lensa-nikon-af-s-50mm-f1.8g.jpg',
       priceDay : 110000
     },
   ]
