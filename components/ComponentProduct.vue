09aa525c8e534a884d30e4fa9eadcd0e5a2b6217
Shironim <dimasseto.id@gmail.com>
2023-08-07 12:15:55 +0700

    update comp product

--- components/ComponentProduct.vue
+++ components/ComponentProduct.vue
@@ -7,19 +7,30 @@ defineProps({
 })
 </script>
 <template>
-  <div v-for="(camera, index) in dataProduct" :key="index" class="basis-1/4">
-    <div class="border rounded-xl m-2">
+  <div v-for="(camera, index) in dataProduct" :key="index" class="basis-1/4 ">
+    <div class="border rounded-md mr-3 mb-3 overflow-hidden">
       <figure>
-        <NuxtImg :src="camera.img" :alt="camera.name" fit="cover" />
+        <NuxtImg :src="camera.img" :alt="camera.name" class="lg:min-h-[140px] xl:min-h-[180px]" height="180px" width="100%" fit="cover" />
       </figure>
       <figcaption class="flex flex-col">
         <div class="p-4">
-          <h1 class="text-lg font-bold">{{ camera.name }}</h1>
-          <p class="text-sm">Rp. {{ camera.priceDay }} / Hari</p>
+          <h1 class="text-md">{{ camera.name }}</h1>
+          <p class="text-lg font-bold">Rp. {{ camera.priceDay }}K /Day</p>
         </div>
-        <NuxtLink :to="`detail/${camera.slug}`" class="mt-auto">
-          <button class="border p-2 rounded-md text-md"> Detail </button>
-        </NuxtLink>
+        <div class="flex border-t">
+          <NuxtLink :to="`detail/${camera.slug}`" class="basis-1/2 transition-all hover:bg-orange-400 hover:text-white">
+            <button class="p-2 w-full text-md"> 
+              <Icon name="tabler:camera-search" class="mx-2"></Icon>
+              <span>Detail</span>
+            </button>
+          </NuxtLink>
+          <NuxtLink :to="`detail/${camera.slug}`" class="basis-1/2 transition-all hover:bg-red-400 hover:text-white">
+            <button class=" hover:border-red-400 p-2 w-full text-md"> 
+              <Icon name="material-symbols:shopping-cart" class="mx-2"></Icon>
+              <span>Simpan</span>  
+            </button>
+          </NuxtLink>
+        </div>
       </figcaption>
     </div>
   </div>
