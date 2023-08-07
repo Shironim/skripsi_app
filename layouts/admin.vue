e5ef392b297d7654442a1c2189b13a3ce10776c4
Shironim <dimasseto.id@gmail.com>
2023-08-07 12:16:47 +0700

    update layout

---
+++ layouts/admin.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from 'vue'
+
+</script>
+
+<template>
+  <AdminNavbar />
+  <AdminSidebar />
+  
+  <div class="p-4 sm:ml-64 mt-12 ">
+    <slot />
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,19 @@ import { inject } from 'vue'
 
 let {isChatActive} = inject('chat')
 
-watchEffect(() => {
-  console.log(isChatActive.value)
-})
+// watchEffect(() => {
+//   console.log(isChatActive.value)
+// })
 </script>
 <template>
-  <Navigasi />
-  <slot />
-  <ChatContainer />
+  <div class="flex">
+    <div :class="isChatActive ? 'basis-8/12' : 'basis-full'">
+      <Navigasi />
+      <slot />
+    </div>
+    <div :class="isChatActive ? 'basis-4/12' : 'hidden'">
+      <ChatContainer />
+    </div>
+  </div>
+  <ChatToogle/>
 </template>
