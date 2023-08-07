c5b8196979b0244b54f9a3addaf1e581954aa60f
Shironim <dimasseto.id@gmail.com>
2023-08-07 12:15:31 +0700

    update comp categories

--- components/Categories.vue
+++ components/Categories.vue
@@ -1,10 +1,45 @@
 <template>
-  <section class="md:max-w-5xl mx-auto mb-12">
-    <div class="flex">
-      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Kamera</span>
-      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Lensa</span>
-      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Aksesoris</span>
-      <span class="px-4 py-2 border rounded-xl text-neutral-500 mx-3">Lampu</span>
+  <section class="md:max-w-5xl mx-auto mb-4 px-4 xl:px-0">
+    <h2 class="text-2xl font-semibold pb-2 mb-2">Kategori Pilihan</h2>
+    <div class="relative w-full">
+      <div class="flex flex-wrap whitespace-nowrap overflow-x-hidden">
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="fluent:border-all-16-filled" class="self-center mr-2"></Icon>
+          <span class="self-center">Semua</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="clarity:camera-solid" class="self-center mr-2"></Icon>
+          <span class="self-center">Kamera Foto</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="typcn:video" class="self-center mr-2"></Icon>
+          <span class="self-center">Kamera Video</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="bxl:zoom" class="self-center mr-2"></Icon>
+          <span class="self-center">Paket Streaming</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="ic:round-camera" class="self-center mr-2"></Icon>
+          <span class="self-center">Lensa</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="mdi:mixer-settings" class="self-center mr-2"></Icon>
+          <span class="self-center">Mixer Video</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="mdi:cast-audio" class="self-center mr-2"></Icon>
+          <span class="self-center">Audio</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="mdi:radio-handheld" class="self-center mr-2"></Icon>
+          <span class="self-center">Komunikasi</span>
+        </div>
+        <div class="border rounded-xl text-neutral-500 mb-3 mr-3 px-3 py-1">
+          <Icon name="icon-park-solid:projector-two" class="self-center mr-2"></Icon>
+          <span class="self-center">TV & Layar</span>
+        </div>
+      </div>
     </div>
   </section>
 </template>
