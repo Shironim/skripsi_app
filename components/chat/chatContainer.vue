c62b8820dc0f004a3310d9ca2855c5d3073ab5da
Shironim <dimasseto.id@gmail.com>
2023-08-07 12:15:11 +0700

    update chat section

--- components/chat/chatContainer.vue
+++ components/chat/chatContainer.vue
@@ -2,51 +2,48 @@
   import { inject } from 'vue'
 
   const {isChatActive} = inject('chat')
+  const {toogle} = inject('chat')
 
 </script>
 <template>
-  <div v-if="isChatActive" class="sticky flex ml-auto mr-4 rounded-md bottom-[110px] w-[400px] h-[400px] bg-white border border-black overflow-hidden">
-    <div class="w-full relative">
-      <header class="flex flex-row border-b-2 border-b-black p-4">
-        <figure class="self-center mr-2">
+  <div v-if="isChatActive" class="fixed bg-white border border-slate-400 overflow-hidden">
+    <div class="w-full relative h-screen">
+      <header class="flex flex-row p-3">
+        <div class="flex">
+          <figure class="self-center mr-2">
           <!-- <img src="" alt=""> -->
-          <div class="w-10 h-10 bg-slate-300 rounded-full"></div>
-        </figure>
-        <div class="flex flex-col self-center">
-          <p class="font-bold">
-            Admin
-          </p>
-          <span class="text-xs">Online</span>
+            <div class="w-10 h-10 bg-slate-300 rounded-full"></div>
+          </figure>
+          <div class="flex flex-col self-center">
+            <p class="font-bold">
+              ChatBot
+            </p>
+            <span class="text-xs">Online</span>
+          </div>
+        </div>
+        <div class="ml-auto">
+          <Icon @click="toogle" name="gg:close-o"></Icon>
         </div>
       </header>
-      <main class="bg-slate-200 h-screen overflow-y-scroll">
-        <div class="p-4">
-          <div class="grid grid-cols-12 mb-4">
-            <div class="col-span-10">
+      <main class="bg-slate-100 h-screen overflow-auto p-4">
+        <div class="grid grid-cols-12 mb-4 bg-white pl-3 py-2 rounded-md">
+          <div class="col-span-10">
+            <p class="text-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, pariatur saepe. Sapiente minus ducimus distinctio nemo veniam rerum quam. 
+            </p>
+          </div>
+        </div>
+        <div class="grid grid-cols-12 mb-4 bg-white pr-3 py-2 rounded-md">
+            <div class="col-start-3 col-span-12">
               <p class="text-xs">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, pariatur saepe. Sapiente minus ducimus distinctio nemo veniam rerum quam. 
               </p>
             </div>
-          </div>
-          <div class="grid grid-cols-12 mb-4">
-            <div class="col-start-2 col-span-10">
-              <p class="text-xs">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              </p>
-            </div>
-          </div>
-          <div class="grid grid-cols-12 mb-4">
-            <div class="col-span-10">
-              <p class="text-xs">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, pariatur saepe. Sapiente minus ducimus distinctio nemo veniam rerum quam. 
-              </p>
-            </div>
-          </div>
         </div>
       </main>
       <footer class="absolute bottom-0 w-full">
         <div class="flex flex-row">
-          <input type="text" class=" w-full border-none" placeholder="ini input" />
+          <input type="text" class="w-full border-none focus:outline-none focus:ring-1 focus:ring-transparent" placeholder="ini input" />
           <button class="p-2 bg-slate-300">
             <Icon name="material-symbols:send"></Icon>
           </button>
@@ -54,5 +51,4 @@
       </footer>
     </div>
   </div>
-  <ChatToogle/>
 </template>
--- components/chat/chatToogle.vue
+++ components/chat/chatToogle.vue
@@ -2,9 +2,10 @@
   import { inject } from 'vue'
 
   let {toogle} = inject('chat')
+  let {isChatActive} = inject('chat')
 </script>
 <template>
-  <div class="sticky bottom-10 flex justify-end mr-4">
+  <div v-if="!isChatActive" class="sticky bottom-10 flex justify-end mr-4">
     <div @click="toogle" class="bg-lime-900 rounded-full p-4 flex flex-row">
       <Icon name="material-symbols:android-messages" class="text-white mx-2 self-center"></Icon>
       <p class="text-lg self-center font-bold text-white">Chat</p>
