5c25352ba0b9661bfe00de00adb3030760332f58
Shironim <dimasseto.id@gmail.com>
2023-09-20 00:55:32 +0700

    Update search comp

--- components/Search.vue
+++ components/Search.vue
@@ -11,20 +11,11 @@ defineProps({
 </script>
 
 <template>
-  <section class="md:max-w-5xl mx-auto mb-4 px-4">
-    <div class="flex">
-      <div class="flex border rounded-md border-black basis-1/2 px-2 py-1">
-        <input type="text" placeholder="Search" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
-          class="w-full border-none bg-transparent focus:border-2 focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-transparent">
-        <Icon name="icon-park-outline:search" class="self-center mr-2"></Icon>
-      </div>
-      <!-- <div class="self-center mx-4 bg-lime-900 px-2 py-3 rounded-md">
-        <Icon name="material-symbols:android-messages" class="text-white mr-2 self-center"></Icon>
-        <span class="text-white font-bold">Chat</span>
-      </div> -->
-      <!-- <div class="self-center mx-4">
-        Ditemukan <span class="font-bold">12 item</span> terkait
-      </div> -->
+  <section class="w-full ">
+    <div class="flex border rounded-md border-black basis-1/2 px-2 py-1">
+      <input type="text" placeholder="Search" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
+        class="w-full border-none bg-transparent focus:border-2 focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-transparent">
+      <Icon name="icon-park-outline:search" class="self-center mr-2"></Icon>
     </div>
   </section>
 </template>
