bd9f6e49315ef0f21f1367f2ee0f2ad23df916c9
Shironim <dimasseto.id@gmail.com>
2024-02-16 20:33:33 +0700

    feat: update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/image-edge',
-    '@nuxtjs/google-fonts',
-    'nuxt-icon',
-    '@tailvue/nuxt',
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image-edge",
+    "@nuxtjs/google-fonts",
+    "nuxt-icon",
+    "@tailvue/nuxt",
+    "@nuxt/image",
   ],
 
   ssr: false,
 
   alias: {
-    '@assets': '/assets',
-    '@plugins': '/plugins',
+    "@assets": "/assets",
+    "@plugins": "/plugins",
+    "@comps": "/composables",
   },
 
-  css: [
-    '@/assets/stylesheets/index.scss'
-  ],
+  css: ["@/assets/stylesheets/index.scss"],
   runtimeConfig: {
     // Config within public will be also exposed to the client
     public: {
-      BASE_API_URL: 'http://localhost:5000',
-    }
+      BASE_API_URL: process.env.BASE_API_URL,
+      IMAGE_URL: process.env.IMAGE_URL,
+      API_RASA: process.env.API_RASA
+    },
   },
 
   googleFonts: {
     families: {
       Roboto: true,
-      'Open+Sans': true,
+      "Open+Sans": true,
       Montserrat: [400, 500],
       Poppins: {
         wght: [300, 400, 500],
-        ital: [400]
+        ital: [400],
       },
     },
-    display: 'swap',
+    display: "swap",
     prefetch: false,
     preconnect: false,
     preload: false,
@@ -44,10 +46,10 @@ export default defineNuxtConfig({
   },
 
   build: {
-    transpile: ['@vuepic/vue-datepicker']
+    transpile: ["@vuepic/vue-datepicker"],
   },
 
   devtools: {
     enabled: false,
   },
-})
+});
