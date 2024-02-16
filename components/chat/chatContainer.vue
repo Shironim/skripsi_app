29d0f08f298c5bd892c2a15339d10109f3bd54a2
Shironim <dimasseto.id@gmail.com>
2024-02-16 20:34:58 +0700

    feat : component chat

--- components/chat/chatContainer.vue
+++ components/chat/chatContainer.vue
@@ -1,64 +1,129 @@
 <script setup>
 import { inject } from "vue";
 
-const { isChatActive } = inject("chat");
-const { toogle } = inject("chat");
-const historyChat = reactive([
-  {
-    bot: false,
-    message: "Hi, Selamat siang",
-  },
-  {
-    bot: true,
-    message: "Selamat datang, boleh dijelaskan apa kebutuhannya ?",
-  },
-]);
+const { isChatActive, toogle } = inject("chat");
+const { historyChat, saveHistory } = inject("historyChat");
+const { isChatLoading, toogleChatLoading } = inject("isChatLoading");
 const inputChat = ref("");
-const baseApiUrl = useRuntimeConfig().public.API_RASA;
+const baseApiUrl = useRuntimeConfig().public.BASE_API_URL;
+const productSearch = ref();
 
-const submitInput = async () => {
+const userToken = localStorage.getItem("usr-token");
+import { useFormatCurrency } from "@comps/useFormatCurrency";
+
+const { formatCurrencyIDR } = useFormatCurrency();
+const submitInput = async (input) => {
   try {
-    const upload = {
-      sender: "user",
-      message: inputChat.value,
+    toogleChatLoading(true);
+    const request = {
+      sender: userToken,
+      message: input,
     };
-    const { error, data } = await useFetch(
+    const { error, data: chat } = await useFetch(
       `http://localhost:5005/webhooks/rest/webhook`,
       {
         method: "POST",
-        body: upload,
+        body: request,
       }
     );
-    historyChat.push({
-      bot: false,
-      message: inputChat.value,
+    saveHistory({
+      isBot: false,
+      message: request.message,
     });
-    if (data.value.length == 1) {
-      historyChat.push({
-        bot: true,
-        message: data.value[0].text,
-      });
-    } else if (data.value.length > 1) {
-      for (let index = 0; index < data.value.length; index++) {
-        historyChat.push({
-          bot: true,
-          message: data.value[index].text,
+    if (chat.value.length > 1) {
+      for (let index = 0; index < chat.value.length; index++) {
+        if (chat.value[index].hasOwnProperty("custom")) {
+          productSearch.value = chat.value[index].custom.data;
+          for (
+            let customIndex = 0;
+            customIndex < chat.value[index].custom.data.length;
+            customIndex++
+          ) {
+            saveHistory({
+              isBot: true,
+              message: chat.value[index].custom.data[customIndex],
+            });
+          }
+        }
+        saveHistory({
+          isBot: true,
+          message: chat.value[index].text,
         });
       }
+    } else if (chat.value.length == 1) {
+      saveHistory({
+        isBot: true,
+        message: chat.value[0].text,
+      });
     }
-    inputChat.value = "";
   } catch (error) {
     console.log("log error", error);
+  } finally {
+    toogleChatLoading(false);
+    inputChat.value = "";
   }
 };
+const addProductToCart = async (produk) => {
+  try {
+    toogleChatLoading(true);
+    const request = {
+      sender: userToken,
+      message: `Saya mau menambah ${produk} ke keranjang belanja`,
+    };
+    const { error, data: chat } = await useFetch(
+      `http://localhost:5005/webhooks/rest/webhook`,
+      {
+        method: "POST",
+        body: request,
+      }
+    );
+    saveHistory({
+      isBot: false,
+      message: request.message,
+    });
+    if (chat.value.length > 1) {
+      for (let index = 0; index < chat.value.length; index++) {
+        if (chat.value[index].hasOwnProperty("custom")) {
+          for (
+            let customIndex = 0;
+            customIndex < chat.value[index].custom.data.length;
+            customIndex++
+          ) {
+            saveHistory({
+              isBot: true,
+              message: chat.value[index].custom.data[customIndex],
+            });
+          }
+        }
+        saveHistory({
+          isBot: true,
+          message: chat.value[index].text,
+        });
+      }
+    } else if (chat.value.length == 1) {
+      saveHistory({
+        isBot: true,
+        message: chat.value[0].text,
+      });
+    }
+  } catch (error) {
+    console.log("log error", error);
+  } finally {
+    toogleChatLoading(false);
+  }
+};
+
+watchEffect(() => {
+  console.log(productSearch.value);
+});
 </script>
 <template>
   <div
     v-if="isChatActive"
-    class="fixed min-w-[341px] bg-white border border-slate-400 overflow-hidden"
+    class="fixed overflow-hidden bg-white rounded-md border w-[500px] h-[600px] bottom-28 right-3 flex justify-end mr-4"
   >
-    <div class="w-full relative h-screen">
-      <header class="flex flex-row p-3">
+    <div class="relative w-full bg-slate-100">
+      <header class="flex flex-row p-3 border-b bg-white">
         <div class="flex">
           <figure class="self-center mr-2">
             <!-- <img src="" alt=""> -->
@@ -73,61 +138,108 @@ const submitInput = async () => {
             <span class="text-xs">Online</span>
           </div>
         </div>
-        <div class="ml-auto">
-          <Icon @click="toogle" name="gg:close-o"></Icon>
+        <div class="ml-auto cursor-pointer">
+          <Icon @click="toogle(false)" name="gg:close-o"></Icon>
         </div>
       </header>
-      <main class="bg-slate-100 h-screen overflow-auto p-4">
+      <main class="overflow-auto p-4 h-[450px]">
         <div
           v-for="(data, index) in historyChat"
           class="flex mb-4"
-          :class="data.bot ? 'even:justify-end' : ''"
+          :class="data.isBot ? 'justify-end' : 'justify-start'"
         >
-          <p class="text-xs w-3/4 bg-white pl-3 pr-4 py-2 rounded-md">
+          <p
+            v-if="typeof data.message === 'string'"
+            class="text-xs w-auto bg-white pl-3 pr-4 py-2 rounded-md"
+            :class="data.isBot ? 'ml-12' : ''"
+          >
             {{ data.message }}
           </p>
+          <div
+            v-if="typeof data.message === 'object'"
+            class="text-xs w-auto bg-white pl-3 pr-4 py-2 rounded-md"
+            :class="data.isBot ? 'ml-12' : ''"
+          >
+            <div>
+              <p class="pb-2 text-md">{{ data.message.nama }}</p>
+              <NuxtImg
+                :src="`${baseApiUrl}/${data.message.thumbnail}`"
+                width="80px"
+                height="80px"
+                class="pb-2"
+              />
+              <div class="flex flex-col">
+                <p class="font-bold text-md mb-2">
+                  {{ formatCurrencyIDR(data.message.harga) }}
+                </p>
+                <button
+                  @click="addProductToCart(data.message.nama)"
+                  class="bg-green-400 px-4 py-1 rounded-sm text-white"
+                >
+                  Pilih
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <!-- <div class="flex mb-4 odd:justify-start">
-          <p class="text-xs w-3/4 bg-white pl-3 pr-4 py-2 rounded-md">
-            Hi, Selamat siang
-          </p>
-        </div>
-        <div class="flex mb-4 even:justify-end">
-          <p class="text-xs w-3/4 bg-white pl-3 pr-4 py-2 rounded-md">
-            Selamat datang, boleh dijelaskan apa kebutuhannya ?
-          </p>
-        </div>
-        <div class="flex mb-4 odd:justify-start">
-          <p class="text-xs w-3/4 bg-white pl-3 pr-4 py-2 rounded-md">
-            Apa saja syarat menyewa kamera ? 
-          </p>
-        </div>
-        <div class="flex mb-4 even:justify-end">
-          <p class="text-xs w-3/4 bg-white pl-3 pr-4 py-2 rounded-md">
-            Untuk syarat sewa nya cukup perlu meninggalkan sementara KTP yaa.
-          </p>
-        </div>
-        <div class="flex mb-4 odd:justify-start">
-          <p class="text-xs w-3/4 bg-white pl-3 pr-4 py-2 rounded-md">
-            Baik, Terimakasih 
-          </p>
-        </div>
-        <div class="flex mb-4 even:justify-end">
-          <p class="text-xs w-3/4 bg-white pl-3 pr-4 py-2 rounded-md">
-            Siap, saya siap membantu
-          </p>
-        </div> -->
       </main>
-      <footer class="absolute bottom-0 w-full">
-        <div class="flex flex-row">
+      <footer class="absolute bg-white bottom-0 w-full p-2 pb-3">
+        <div class="pb-2">
+          <div class="flex overflow-x-auto whitespace-nowrap gap-x-2">
+            <button
+              @click="submitInput('lihat keranjang belanja')"
+              class="flex text-green-500 justify-center text-xs text-center flex-nowrap overflow-hidden whitespace-nowrap cursor-pointer h-[32px] border px-2 py-1 border-green-400 rounded-full"
+            >
+              <span
+                class="self-center max-w-[120px] whitespace-nowrap overflow-hidden text-ellipsis"
+                >lihat keranjang belanja dong</span
+              >
+            </button>
+            <button
+              @click="submitInput('Apa saja ketentuan sewa ?')"
+              class="flex text-green-500 justify-center text-xs text-center flex-nowrap overflow-hidden whitespace-nowrap cursor-pointer h-[32px] border px-2 py-1 border-green-400 rounded-full"
+            >
+              <span
+                class="self-center max-w-[120px] whitespace-nowrap overflow-hidden text-ellipsis"
+                >Apa saja ketentuan sewa ?</span
+              >
+            </button>
+            <button
+              @click="
+                submitInput('Pembayarannya apakah bisa transfer atau cash ?')
+              "
+              class="flex text-green-500 justify-center text-xs text-center flex-nowrap overflow-hidden whitespace-nowrap cursor-pointer h-[32px] border px-2 py-1 border-green-400 rounded-full"
+            >
+              <span
+                class="self-center max-w-[120px] whitespace-nowrap overflow-hidden text-ellipsis"
+                >Pembayarannya apakah bisa transfer atau cash ?</span
+              >
+            </button>
+            <div
+              class="rounded-full justify-center ml-auto cursor-pointer self-center"
+            >
+              <Icon
+                name="mingcute:up-fill"
+                class="text-green-400 mx-auto w-[32px] self-center"
+              ></Icon>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row gap-x-2">
           <input
             type="text"
             v-model="inputChat"
-            class="w-full border-none focus:outline-none focus:ring-1 focus:ring-transparent"
+            class="w-full border-green-400 focus:border-green-400 focus:outline-none focus:ring focus:ring-green-400 rounded-full"
             placeholder="Tanyakan pada chatbot"
           />
-          <button @click="submitInput" class="p-2 bg-green-400">
-            <Icon name="material-symbols:send" class="text-white"></Icon>
+          <button class="bg-green-400 px-1 rounded-full justify-center">
+            <Icon
+              v-if="!isChatLoading"
+              @click="submitInput(inputChat)"
+              name="material-symbols:send"
+              class="text-white mx-auto w-[32px] self-center"
+            ></Icon>
+            <div v-if="isChatLoading" class="loader"></div>
           </button>
         </div>
       </footer>
--- components/chat/chatToogle.vue
+++ components/chat/chatToogle.vue
@@ -1,13 +1,15 @@
 <script setup>
-  import { inject } from 'vue'
-  //  inject chat
-  let {toogle} = inject('chat')
-  let {isChatActive} = inject('chat')
+import { inject } from "vue";
+//  inject chat
+let { toogle, isChatActive } = inject("chat");
 </script>
 <template>
-  <div v-if="!isChatActive" class="fixed bottom-10 right-3 flex justify-end mr-4">
-    <div @click="toogle" class="bg-lime-900 rounded-full p-4 flex flex-row">
-      <Icon name="material-symbols:android-messages" class="text-white mx-2 self-center"></Icon>
+  <div v-if="!isChatActive" class="fixed bottom-10 right-3 flex justify-end mr-4 cursor-pointer">
+    <div @click="toogle(true)" class="bg-lime-900 rounded-full p-4 flex flex-row">
+      <Icon
+        name="material-symbols:android-messages"
+        class="text-white mx-2 self-center"
+      ></Icon>
       <p class="text-lg self-center font-bold text-white">Chat</p>
     </div>
   </div>
